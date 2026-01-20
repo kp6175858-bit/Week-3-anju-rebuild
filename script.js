b3ea2731a26c9ab283849cb7e8106a4c33fd45c8
kp6175858-bit <kp6175858@gmail.com>
2026-01-20 21:49:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
   document.getElementById("navMenu").classList.toggle("open");
 }
+const cards = document.querySelectorAll(".info-card, .role-card");
+
+window.addEventListener("scroll", () => {
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    if (position < window.innerHeight - 100) {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
