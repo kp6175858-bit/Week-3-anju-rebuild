4b062832ea3e5b6efa856144540463eaf773b7ce
kp6175858-bit <kp6175858@gmail.com>
2026-01-21 21:18:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const cards = document.querySelectorAll(".slide-up");
+const button = document.querySelector(".btn");
 
-window.addEventListener("scroll", () => {
-  const trigger = window.innerHeight * 0.85;
-
-  cards.forEach(card => {
-    const top = card.getBoundingClientRect().top;
-    if (top < trigger) {
-      card.classList.add("show");
-    }
-  });
+button.addEventListener("click", () => {
+  alert("Week 3 Task Started 🚀");
 });
