977a283ec0ef367549a9a62445d000bf249c80ce
kp6175858-bit <kp6175858@gmail.com>
2026-01-22 20:41:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+const cards = document.querySelectorAll(".service-card");
+
+window.addEventListener("scroll", () => {
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
+
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  alert("Form submitted successfully!");
+});
