b432a7bbf32c00a61cd476afe6d98cb1b26f98a9
kp6175858-bit <kp6175858@gmail.com>
2026-01-23 19:58:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,7 @@ document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
   alert("Form submitted successfully!");
 });
+document.querySelector("form").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Message sent successfully!");
+});
